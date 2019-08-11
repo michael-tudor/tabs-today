df24e64814d86f849ba164ba2819adfb346d02e1
Michael Toderashco <mihail.t@travod.com>
2019-08-11 17:50:45 +0300

    Rename project

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Tabsday
+module Tabstoday
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
